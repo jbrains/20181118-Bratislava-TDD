b72a3c8991cb2d0f88eaa7f206a0668ebf9b471b
J. B. Rainsberger <me@jbrains.ca>
2018-11-20 10:50:01 +0100

    [FAILING TEST] We have described a test for selling multiple items and finding all the products.

--- PointOfSale-Java/src/test/java/ca/jbrains/pos/test/SellMultipleItemsTest.java
+++ PointOfSale-Java/src/test/java/ca/jbrains/pos/test/SellMultipleItemsTest.java
@@ -43,4 +43,21 @@ public class SellMultipleItemsTest {
 
         Assert.assertEquals("No purchase is in progress. Please scan a product.", display.getText());
     }
+
+    @Test
+    public void severalBarcodesScannedAndAllProductsFound() throws Exception {
+        Display display = new Display();
+        Sale sale = new Sale(display, new Catalog(new HashMap<String, String>() {{
+            put("12345", "EUR 7.50");
+            put("23456", "EUR 12.95");
+            put("34567", "EUR 18.65");
+        }}));
+        sale.onBarcode("12345");
+        sale.onBarcode("23456");
+        sale.onBarcode("34567");
+
+        sale.onTotal();
+
+        Assert.assertEquals("Total: EUR 39.10", display.getText());
+    }
 }
