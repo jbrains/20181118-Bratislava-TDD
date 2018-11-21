e59df3d20808abb4347fb892f0b4fec39528fd2d
J. B. Rainsberger <me@jbrains.ca>
2018-11-21 15:21:32 +0100

    Removed obsolete code.

--- PointOfSale-Java/src/main/java/ca/jbrains/pos/RenderTextToConsole.java
+++ PointOfSale-Java/src/main/java/ca/jbrains/pos/RenderTextToConsole.java
@@ -1,9 +1,6 @@
 package ca.jbrains.pos;
 
 public class RenderTextToConsole {
-    public RenderTextToConsole() {
-    }
-
     public void render(String text) {
         System.out.println(text);
     }
