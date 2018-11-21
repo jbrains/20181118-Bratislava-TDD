0d24d0c390c788694a9eece8b9197cb17cb8795b
J. B. Rainsberger <me@jbrains.ca>
2018-11-21 15:47:00 +0100

    Improved formatting.

--- PointOfSale-Java/src/main/java/ca/jbrains/pos/PointOfSaleTerminal.java
+++ PointOfSale-Java/src/main/java/ca/jbrains/pos/PointOfSaleTerminal.java
@@ -4,13 +4,19 @@ import io.vavr.collection.HashMap;
 
 public class PointOfSaleTerminal {
     public static void main(String[] args) {
-        SellOneItemController sellOneItemController = new SellOneItemController(new InMemoryCatalog(HashMap.of(
+        SellOneItemController sellOneItemController = new SellOneItemController(
+                new InMemoryCatalog(
+                        HashMap.of(
                 "4004764391257", Price.euroCents(189),
                 "4004764391264", Price.euroCents(239),
                 "8717868162116", Price.euroCents(229),
                 "8595013615447", Price.euroCents(199)
-        ).toJavaMap()), new TextDisplay(new RenderTextToPrintStream(System.out)));
+        ).toJavaMap()),
+                new TextDisplay(
+                        new RenderTextToPrintStream(System.out)));
 
-        new CommandLineTextClient().streamAsLines(System.in).forEach(sellOneItemController::onBarcode);
+        new CommandLineTextClient()
+                .streamAsLines(System.in)
+                .forEach(sellOneItemController::onBarcode);
     }
 }
